10983f0303fb6865d305f0f54684413b16acaf6b
Dylan Baxter <dylanjaybaxter@gmail.com>
2022-02-25 17:28:48 -0800

    Change Device Number 2

--- Basic_Pi_Test.py
+++ Basic_Pi_Test.py
@@ -6,9 +6,9 @@ while True:
     ret, image = cam.read()
     cv2.imshow('Imtest', image)
     key = cv2.waitKey(1)
-    if key == -1:
+    if key != -1:
         break
-cv2.imwrite('/images/test/testimage.jpg', image)
+cv2.imwrite('/home/pi/testimage.jpg', image)
 cam.release()
 cv2.destroyAllWindows()
 
